7d7aa6f8f7e9ebc2c1ab0166513408cab3fc968e
houyuxi <1938913473@qq.com>
2024-02-12 20:37:34 +0800

    fix: logs command not using tty
    use child_proxy instead of shelljs for -ti option

--- src/commands/cli/index.ts
+++ src/commands/cli/index.ts
@@ -1,9 +1,12 @@
-import type {CmdExport} from "@/lib/fs-cmd-register" ;
-import {t} from "i18next" ;
-import shelljs from "shelljs" ;
+import type { CmdExport } from "@/lib/fs-cmd-register" ;
+import * as child_process from "child_process" ;
+import { t } from "i18next" ;
 
 function action(): void {
-    shelljs.exec("docker exec -ti screeps screeps-launcher cli") ;
+    child_process.execFileSync("docker", ["exec", "-ti", "screeps", "screeps-launcher", "cli"], {
+        shell: true,
+        stdio: "inherit",
+    }) ;
 }
 
 const cli: CmdExport = {
